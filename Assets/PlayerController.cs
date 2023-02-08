a31a9094c8a25d7f645aa8e6802b04fb06118461
1C3T0C0LD <isaacmitsinga@gmail.com>
2023-02-08 10:41:37 +0000

    Finished character,  added camera, started spawner

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public GameObject groundChecker;
+    public LayerMask whatIsGround;
+
+    float maxSpeed  = 5.0f;
+    bool isOnGround = false;
+
     //Create a reference to the Rigidbody2D so we can manipulate it
     Rigidbody2D playerObject;
 
@@ -22,5 +28,14 @@ public class PlayerController : MonoBehaviour
 
         //Change the X velocity of the Rigidbody2D to be equal to the movement value
         playerObject.velocity = new Vector2 (movementValueX, playerObject.velocity.y); 
+
+        //Check to see  if the ground  check object is touching the ground
+        isOnGround= Physics2D.OverlapCircle(groundChecker.transform.position,  1.0f, whatIsGround);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround == true)
+        {
+            playerObject.AddForce(new  Vector2(0.0f, 100.0f));
+        }
+
     }
 }
