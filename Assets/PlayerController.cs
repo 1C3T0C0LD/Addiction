638c021b0f658e1c7f83bf343a9abb39b2d0c09d
1C3T0C0LD <isaacmitsinga@gmail.com>
2023-02-08 12:33:30 +0000

    I finished the script for my spawner I need to make my character faster

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
     public GameObject groundChecker;
     public LayerMask whatIsGround;
 
-    float maxSpeed  = 5.0f;
+    float maxSpeed  = 10.0f;
     bool isOnGround = false;
 
     //Create a reference to the Rigidbody2D so we can manipulate it
@@ -23,8 +23,18 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            maxSpeed = 10.0f;
+        } else
+        {
+            maxSpeed = 10.0f;
+        }
         //Create a 'float' that will be equal to the players horizontal input
-        float movementValueX = Input.GetAxis("Horizontal");
+        //float movementValueX = Input.GetAxis("Horizontal");
+
+        //Set movementValueX to 1.0f, so that we always run forward and no longer care about player input
+        float movementValueX = 1.0f;
 
         //Change the X velocity of the Rigidbody2D to be equal to the movement value
         playerObject.velocity = new Vector2 (movementValueX, playerObject.velocity.y); 
